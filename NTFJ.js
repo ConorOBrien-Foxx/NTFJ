c80d0e3f6bd6217f90473259b8a879c4ba17b475
Conor O'Brien <conorobrien4god@gmail.com>
2016-02-23 08:25:49 -0500

    add a switch command

--- NTFJ.js
+++ NTFJ.js
@@ -1,7 +1,7 @@
 // NTFJ
 // By Conor O'Brien and ETHProductions
 
-var COMMANDS = "|#~^*@`()$:/";
+var COMMANDS = "|#~^*@`()$:/%";
 
 function NTFJStep(code,stack,i){
 	if(i<code.length){
@@ -14,22 +14,23 @@ function NTFJStep(code,stack,i){
 			if(stack[stack.length-1]>1) stack=stack.concat(("00000000"+stack.pop().toString(2)).slice(-8).split("").map(Number))
 			else stack.push(parseInt([0,0,0,0,0,0,0,0].map(function(){return stack.pop()||0}).reverse().join(""),2));
 		}
-    else if(code[i]==7)console.log(stack);
-    else if(code[i]==8){
-    	var d = 1;
-    	if(!stack[stack.length-1]){
-	    	while(i<code.length){
-	    		i++;
-	    		if(code[i]==9) d--;
-	    		else if(code[i]==8) d++;
-	    		if(!d) break;
+	    else if(code[i]==7)console.log(stack);
+	    else if(code[i]==8){
+	    	var d = 1;
+	    	if(!stack[stack.length-1]){
+		    	while(i<code.length){
+		    		i++;
+		    		if(code[i]==9) d--;
+		    		else if(code[i]==8) d++;
+		    		if(!d) break;
+		    	}
 	    	}
-    	}
-    }
-    else if(code[i]==10){stack.pop();}
-    else if(code[i]==11){r=stack.pop();stack.push(r,r)}
-    else if(code[i]==12){stack.push(stack.length)}
-    setTimeout(NTFJStep,1,code,stack,i+1)
+	    }
+	    else if(code[i]==10){stack.pop();}
+	    else if(code[i]==11){r=stack.pop();stack.push(r,r)}
+	    else if(code[i]==12){stack.push(stack.length)}
+	    else if(code[i]==13){var r=stack.pop(),s=stack.pop();stack.push(r,s)}
+	    setTimeout(NTFJStep,1,code,stack,i+1)
   } else {
   	k.disabled = false;
   }
@@ -44,11 +45,12 @@ function NTFJ(code,input){
 		else if(code[i]==4){i=stack.pop()-1;}
 		else if(code[i]==5)d.innerHTML+=String.fromCharCode(stack.pop());
 		else if(code[i]==6)stack.push(parseInt([0,0,0,0,0,0,0,0].map(function(){return stack.pop()||0}).reverse().join(""),2));
-    else if(code[i]==7)console.log(stack);
-    else if(code[i]==8){if(!stack[stack.length-1])while(code[++i]!=9&&i<code.length);}
-    else if(code[i]==10){stack.pop();}
-    else if(code[i]==11){r=stack.pop();stack.push(r,r)}
-    else if(code[i]==12){stack.push(stack.length)}
+	    else if(code[i]==7)console.log(stack);
+	    else if(code[i]==8){if(!stack[stack.length-1])while(code[++i]!=9&&i<code.length);}
+	    else if(code[i]==10){stack.pop();}
+	    else if(code[i]==11){r=stack.pop();stack.push(r,r)}
+	    else if(code[i]==12){stack.push(stack.length)}
+	    else if(code[i]==13){var r=stack.pop(),s=stack.pop();stack.push(r,s)}
 	}
 }
 
